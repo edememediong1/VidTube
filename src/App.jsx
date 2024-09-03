8079a2a1f57d9add2e3068300d94c409196b82ee
Emediong Bassey Edem <edememediong1@gmail.com>
2024-09-03 01:43:23 +0100

    Added styles to the side bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Navbar from './Components/Navbar/Navbar'
 import Home from './Pages/Home/Home'
@@ -6,11 +7,14 @@ import Video from './Pages/Video/Video'
 
 
 const App = () => {
+  
+  const [sidebar, setSideBar] = useState(true);
+
   return (
     <div>
-        <Navbar/>
+        <Navbar setSideBar={setSideBar}/>
         <Routes>
-          <Route path='/' element={<Home/>} />
+          <Route path='/' element={<Home sidebar={sidebar}/>} />
           <Route path='/video/:categoryId/:videoId' element={<Video/>} />
         </Routes>
     </div>
