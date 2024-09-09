b7d55ce33f15634ce0358af9e37e63b375cab0cd
Emediong Bassey Edem <edememediong1@gmail.com>
2024-09-09 18:20:25 +0100

    Added the Jsx for the PlayVideo segment

--- src/Components/PlayVideo/PlayVideo.jsx
+++ src/Components/PlayVideo/PlayVideo.jsx
@@ -1,8 +1,94 @@
-
+import './PlayVideo.css'
+import video1 from '../../assets/video.mp4'
+import like from '../../assets/like.png'
+import dislike from '../../assets/dislike.png'
+import share from '../../assets/share.png'
+import save from '../../assets/save.png'
+import jack from '../../assets/jack.png'
+import user_profile from '../../assets/user_profile.jpg'
 
 function PlayVideo() {
   return (
-    <div>PlayVideo</div>
+    <div>
+        <video src={video1} controls autoPlay muted></video>
+        <h3>Best YouTube Channel to Learn Web development</h3>
+        <div className="play-video-info">
+          <p>1525 Views &bull; 2 days ago</p>
+          <div>
+            <span><img src={like} alt="" />125</span>
+            <span><img src={dislike} alt="" />5</span>
+            <span><img src={share} alt="" />Share</span>
+            <span><img src={save} alt="" />Save</span>
+          </div>
+        </div>
+        <hr />
+        <div className="publisher">
+          <img src={jack} alt="" />
+          <div>
+            <p>GreatStack</p>
+            <span>1M Subscribers</span>
+          </div>
+          <button>Subscribe</button>
+        </div>
+        <div className="vid-description">
+          <p>Channel that makes learning easy</p>
+          <p>Subscribe GreatStack to watch more tutorials on Web development</p>
+          <hr/>
+          <h4>130 Comments</h4>
+          <div className='comment'>
+            <img src={user_profile} alt="" />
+            <div>
+              <h3>Jack Nicholson <span>1 day ago</span></h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut totam libero cumque doloribus perferendis minus aperiam necessitatibus dolor commodi asperiores!</p>
+              <div className="comment-action">
+                <img src={like} alt="" />
+                <span>244</span>
+                <img src={dislike} alt="" />
+              </div>
+            </div>
+          </div>
+
+          <div className='comment'>
+            <img src={user_profile} alt="" />
+            <div>
+              <h3>Jack Nicholson <span>1 day ago</span></h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut totam libero cumque doloribus perferendis minus aperiam necessitatibus dolor commodi asperiores!</p>
+              <div className="comment-action">
+                <img src={like} alt="" />
+                <span>244</span>
+                <img src={dislike} alt="" />
+              </div>
+            </div>
+          </div>
+
+          <div className='comment'>
+            <img src={user_profile} alt="" />
+            <div>
+              <h3>Jack Nicholson <span>1 day ago</span></h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut totam libero cumque doloribus perferendis minus aperiam necessitatibus dolor commodi asperiores!</p>
+              <div className="comment-action">
+                <img src={like} alt="" />
+                <span>244</span>
+                <img src={dislike} alt="" />
+              </div>
+            </div>
+          </div>
+
+          <div className='comment'>
+            <img src={user_profile} alt="" />
+            <div>
+              <h3>Jack Nicholson <span>1 day ago</span></h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut totam libero cumque doloribus perferendis minus aperiam necessitatibus dolor commodi asperiores!</p>
+              <div className="comment-action">
+                <img src={like} alt="" />
+                <span>244</span>
+                <img src={dislike} alt="" />
+              </div>
+            </div>
+          </div>
+
+        </div>
+    </div>
   )
 }
 
--- src/Pages/Video/Video.jsx
+++ src/Pages/Video/Video.jsx
@@ -1,10 +1,11 @@
 
 import './Video.css'
+import PlayVideo from '../../Components/PlayVideo/PlayVideo'
 
 function Video() {
   return (
     <div className='play-container'>
-      
+        <PlayVideo/>
     </div>
   )
 }
